21357924b9270ca5b1310d54a96e822434961204
Juan Manuel <jumarana1007@gmail.com>
2025-05-09 21:10:36 -0500

    Update logica.py

--- logica.py
+++ logica.py
@@ -1,11 +1,11 @@
 from ejercicios import saludo, suma, par_impar, mayor_edad, mayor_lista, contar_letras, area_triangulo, grados_fahrenheit
 
 
-#saludo()
-#suma()
-#par_impar()
-#mayor_edad()
-#contar_letras()
-#mayor_lista()
-#area_triangulo('base', 'altura')
+saludo()
+suma()
+par_impar()
+mayor_edad()
+contar_letras()
+mayor_lista()
+area_triangulo('base', 'altura')
 grados_fahrenheit('celsius')
